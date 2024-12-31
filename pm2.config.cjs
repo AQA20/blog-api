e311a47201471ad850cbd40b36ae7fad9355c772
Ahmed <ahmedqss120@gmail.com>
2025-01-01 02:03:45 +0300

    Refactor .env file creation to use echo commands for improved clarity and maintainability; remove unused DB_ROOT_PASSWORD from pm2 config

--- pm2.config.cjs
+++ pm2.config.cjs
@@ -24,7 +24,6 @@ module.exports = {
         // Database Configuration
         DB_USERNAME: process.env.DB_USERNAME,
         DB_PASSWORD: process.env.DB_PASSWORD,
-        DB_ROOT_PASSWORD: process.env.DB_ROOT_PASSWORD,
         DB_NAME: process.env.DB_NAME,
         DB_HOST: process.env.DB_HOST,
         DB_PORT: process.env.DB_PORT,
