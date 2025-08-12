6fde438bbb174f8625bc3df84caf9839d35d2468
Ahmed <ahmedqss120@gmail.com>
2025-08-12 13:32:20 +0300

    Update domain extension from .com to .site

--- config/corsConfig.js
+++ config/corsConfig.js
@@ -1,13 +1,13 @@
 const allowedOrigins = [
-  'https://500kalima.com',
-  'https://test.500kalima.com',
+  'https://500kalima.site',
+  'https://test.500kalima.site',
   'http://localhost:3000',
   'http://localhost:3001',
   'http://localhost:3002',
   'http://localhost:3003',
-  'https://www.500kalima.com',
-  'https://admin.500kalima.com',
-  'https://manage.500kalima.com',
+  'https://www.500kalima.site',
+  'https://admin.500kalima.site',
+  'https://manage.500kalima.site',
 ];
 
 const corsOptions = {
--- utils/constants.js
+++ utils/constants.js
@@ -3,7 +3,7 @@ export const REFRESH_TOKEN_EXPIRATION_TIME = '7d';
 export const ACCESS_TOKEN_MAX_AGE_MS = 15 * 60 * 1000;
 export const REFRESH_TOKEN_MAX_AGE_MS = 7 * 24 * 60 * 60 * 1000;
 export const DOMAIN =
-  process.env.NODE_ENV === 'development' ? '.localhost' : '.500kalima.com';
+  process.env.NODE_ENV === 'development' ? '.localhost' : '.500kalima.site';
 export const SECURE = process.env.NODE_ENV === 'development' ? false : true;
 //For production environments when using cross-site requests over HTTPS.
 //'SameSite=None' allows the cookie to be sent in cross-site requests, and
